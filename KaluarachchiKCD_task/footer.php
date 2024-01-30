60adcbd3d691e8b166b21c10a8276989d85a4e38
KaluarachchiKCD <dinusha4010@gmail.com>
2024-01-30 14:16:57 +0200

    Exercise 1 completed

---
+++ KaluarachchiKCD_task/footer.php
@@ -0,0 +1,19 @@
+</div>
+    
+
+  
+  <div class="container text-center bgcolor3">
+  <div class="row">  
+  <div class="col-sm-4">&copy; Dinusha Kaluarachchi</div>
+  <div class="col-sm-4">2024</div>
+  <div class="col-sm-4">All Right reseverd </div>
+ 
+
+  
+
+
+  </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
--- KaluarachchiKCD_task/header.php
+++ KaluarachchiKCD_task/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title><?php echo "$title" ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
   </head>
@@ -61,63 +61,3 @@
 </div>
     <div class="col-sm-9 text-start bgcolor2" >
 
-    <h3>3.1 Write PHP code to output the following message:<br> Hello world! My name is "David"</h3>
-    
-<?php
-echo "Hello world! My name is \"David\" ";
-?>
-
-<h3>Create a PHP variable named $title and assign it 
-    the value "PHP is interesting." 
-    Then, use this variable as the content within an  (heading 4) element.</h3>
-
-    <?php
-    $title="PHP is interesting." ;
-    echo "<h4>$title</h4>";
-
-    ?>
-
-    <h3>3.3Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables
-         represent the grades of three students in the course. To display this information,
-          create an HTML table within your PHP code. The table should be structured with columns
-           for Serial Number (S.n.), Name, and Grade, and it should look like this:</h3>
-
-    <?php
-
-    $g1=5;
-    $g2=4;
-    $g3=5;
-
-
-
-    ?>
-    <table>
-        <tr><th>S.n.</th><th>Name</th><th>Grade</th></tr>
-        <tr><td>1</td><td>John</td><td><?php echo $g1 ?></td></tr>
-        <tr><td>2</td><td>Alice</td><td><?php echo $g2 ?></td></tr>
-        <tr><td>3</td><td>Bob</td><td><?php echo $g3 ?></td></tr>
-    </table>
-
-    <h3>3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3>
-    <img src="./Image/24.png" class="img-fluid" alt="Web development environment">
-
-
-    </div>
-    
-
-  
-  <div class="container text-center bgcolor3">
-  <div class="row">  
-  <div class="col-sm-4">&copy; Dinusha Kaluarachchi</div>
-  <div class="col-sm-4">2024</div>
-  <div class="col-sm-4">All Right reseverd </div>
- 
-
-  
-
-
-  </div>
-  </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
